611e5818975876a3444c8f00df3aecbbaa111a11
isha0612 <isha06dec@gmail.com>
2022-02-21 01:37:43 +0530

    added local storage to deleted array

--- src/Context/DataState.jsx
+++ src/Context/DataState.jsx
@@ -9,15 +9,21 @@ const DataState = (props) => {
 
   useEffect(() => {
     const savedNotes = JSON.parse(localStorage.getItem("react-notes-app-data"));
+    const delNotes = JSON.parse(localStorage.getItem("react-del-notes-data"));
 
-    if (savedNotes) {
+    if(savedNotes) {
       setNotes(savedNotes);
+    }
+
+    if(delNotes) {
+      setDelNotes(delNotes);
     }
   }, []);
 
   useEffect(() => {
     localStorage.setItem("react-notes-app-data", JSON.stringify(notes));
-  }, [notes]);
+    localStorage.setItem("react-del-notes-data",JSON.stringify(dNotes));
+  }, [notes, dNotes]);
 
   const addNote = (info) => {
     setNotes((prev) => {
